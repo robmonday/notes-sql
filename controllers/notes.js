21c12aefd92c83335403e4988affef58b6fc2a09
“robmonday” <10903531+robmonday@users.noreply.github.com>
2023-01-16 23:09:37 -0500

    application structuring complete

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,17 +2,22 @@ const router = require('express').Router()
 
 const { Note } = require('../models')
 
+const noteFinder = async (req, res, next) => {
+  req.note = await Note.findByPk(req.params.id)
+  next()
+}
+
 router.get('/', async (req, res) => {
   const notes = await Note.findAll()
   console.log(notes.map((note) => JSON.stringify(note, null, 2)))
   res.json(notes)
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', noteFinder, async (req, res) => {
   const note = await Note.findByPk(req.params.id)
-  if (note) {
-    console.log(JSON.stringify(note, null, 2))
-    res.json(note)
+  if (req.note) {
+    console.log(JSON.stringify(req.note, null, 2))
+    res.json(req.note)
   } else {
     res.status(404).end()
   }
@@ -27,12 +32,18 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.put('/:id', async (req, res) => {
-  const note = await Note.findByPk(req.params.id)
-  if (note) {
-    note.important = req.body.important
-    await note.save()
-    res.json(note)
+router.delete('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    await req.note.destroy()
+  }
+  res.status(204).end()
+})
+
+router.put('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    req.note.important = req.body.important
+    await req.note.save()
+    res.json(req.note)
   } else {
     res.status(404).end()
   }
